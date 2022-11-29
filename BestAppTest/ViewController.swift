a5b9ba15918a3eb7bd3147fbf8c156da07a99d5e
Karim Sadykov <rfhbv7ic@icloud.com>
2022-11-29 18:38:42 +0300

    Initial Commit

---
+++ BestAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestAppTest
+//
+//  Created by Карим Садыков on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
